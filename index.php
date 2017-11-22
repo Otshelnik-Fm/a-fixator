0e4552925ec6ef94fd69d686182c4679dbf0040b
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2017-11-22 23:53:39 +0400

    v1.1

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-function af_connect_functions(){
-    $my_functions = RCL_TAKEPATH.'templates/a-fixator-functions.php';
-    if (file_exists($my_functions)) {
-        require_once($my_functions);
-    }
+// подключаем файл функций
+$my_functions = RCL_TAKEPATH.'templates/a-fixator-functions.php';
+if (file_exists($my_functions)) {
+    require_once($my_functions);
 }
-add_action('init', 'af_connect_functions');
 
 
 // подключаем стили фронтенда (и реколл минимизация стилей поддерживается)
@@ -14,7 +12,7 @@ function af_connect_front_style(){
     if(!is_admin()){
         $my_front_style = RCL_TAKEPATH.'templates/a-fixator-front.css';
         if (file_exists($my_front_style)) {
-            rcl_enqueue_style('af_front_style',content_url('wp-recall/templates/a-fixator-front.css'));
+            rcl_enqueue_style('af_front_style',content_url('wp-recall/templates/a-fixator-front.css'), true); // 3 аргумент - грузить в подвале
         }
     }
 }
